6229ef085f9ddd628a41dc60b139eef9b3b769d6
marco antonio <valeriomartinezmarco@gmail.com>
2023-09-03 21:58:11 -0600

    formulario3a

--- app/Views/registroCasa.php
+++ app/Views/registroCasa.php
@@ -58,11 +58,19 @@ include_once "template/header.php";
                         <span class="bs-stepper-label">Logins</span>
                       </button>
                     </div>
-                    
+
+                    <div class="step" data-target="#bb-part">
+                      <button type="button" class="step-trigger" role="tab" aria-controls="bb-part" id="bb-part-trigger">
+                        <span class="bs-stepper-circle">3</span>
+                        <span class="bs-stepper-label">Logins</span>
+                      </button>
+                    </div>
+
+
                     <div class="line"></div>
                       <div class="step" data-target="#information-part">
                         <button type="button" class="step-trigger" role="tab" aria-controls="information-part" id="information-part-trigger">
-                          <span class="bs-stepper-circle">3</span>
+                          <span class="bs-stepper-circle">4</span>
                           <span class="bs-stepper-label">Various information</span>
                         </button>
                       
@@ -97,6 +105,18 @@ include_once "template/header.php";
                     </div>
 
 
+                    <div id="bb-part" class="content" role="tabpanel" aria-labelledby="bb-part-trigger">
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                      </div>
+                      <button class="btn btn-primary" onclick="stepper.previous()">Previous</button>
+                      <button class="btn btn-primary" onclick="stepper.next()">Next</button>
+                    </div>
 
 
                     <div id="information-part" class="content" role="tabpanel" aria-labelledby="information-part-trigger">
